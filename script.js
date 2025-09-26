6ef3d477acf168aadb2355683a0407b8a49c2489
Djaria UWASE <uwasedjaria41@gmail.com>
2025-09-26 20:35:55 +0200

    accessing elements in js

---
+++ script.js
@@ -0,0 +1,6 @@
+const head=document.getElementById("head");
+const form=document.getElementById("form");
+const taskNameInput=document.getElementById("taskName");
+const dueDateInput=document.getElementById("taskName");
+const addTaskBtn=document.getElementById("taskBtn");
+const taskList=document.getElementById("taskList");

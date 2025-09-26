c0cdb15b4c12ec28b3ab8d2ba40e65f71d4bce49
Djaria UWASE <uwasedjaria41@gmail.com>
2025-09-26 22:12:05 +0200

    Add task to task list and clear input fields

--- script.js
+++ script.js
@@ -5,13 +5,19 @@ const dueDateInput=document.getElementById("dueDate");
 const addTaskBtn=document.getElementById("taskBtn");
 
 
-form.addEventListener('click',
+form.addEventListener('submit',
     function(e){
      e.preventDefault();
 
-     if(taskNameInput.value.trim()==="")
+     if(taskNameInput.value.trim()===""){
         alert("Task Name is required");
     return;
      }
+ const taskList=document.getElementById("taskList") ;  
+ const li=document.createElement("li");
+ li.textContent=taskNameInput.value + "-" + dueDateInput.value;
+ taskList.appendChild(li);
 
-);
+ taskNameInput.value="";
+ dueDateInput.value="";
+});

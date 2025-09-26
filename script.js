e716031f1a82ebdb46c792c9beba058f552c4eba
Djaria UWASE <uwasedjaria41@gmail.com>
2025-09-26 21:58:09 +0200

    add README.md file,Refactor code and add form submit event listerner

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const head=document.getElementById("head");
 const form=document.getElementById("form");
 const taskNameInput=document.getElementById("taskName");
-const dueDateInput=document.getElementById("taskName");
+const dueDateInput=document.getElementById("dueDate");
 const addTaskBtn=document.getElementById("taskBtn");
-const taskList=document.getElementById("taskList");
+
+
+form.addEventListener('click',
+    function(e){
+     e.preventDefault();
+
+     if(taskNameInput.value.trim()==="")
+        alert("Task Name is required");
+    return;
+     }
+
+);
